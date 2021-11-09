cd16ad8f8801486a563fd4f4a0a2dc37ba951971
kazunok0 <rin.kazunok0@gmail.com>
2021-11-08 20:39:16 -0800

    add generousNumber function

--- src/index.js
+++ src/index.js
@@ -13,9 +13,18 @@ module.exports = {
         if (typeof value === 'string'){
             return value.length;
         }
-        else if (typeof value ==='number'){
+        else if (typeof value === 'number'){
             return value;
         }
         return -1;
+    },
+    generousNumber: (value) => {
+        if(typeof value =='number'){
+            return Math.abs(value)*2;
+        }
+        else if (typeof value === 'string'){
+            return value.length*2;
+        }
+        return 1000;
     }
 };
